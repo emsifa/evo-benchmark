5b9b7e54043d8e38a2610f1a2a6103f8ce7b7b8d
Muhammad Syifa <emsifa@gmail.com>
2021-09-25 20:57:10 +0700

    Fix base url in benchmark evo

--- benchmark-evo.sh
+++ benchmark-evo.sh
@@ -11,25 +11,25 @@ php artisan route:clear
 
 echo ""
 echo "[evo] Running curl to POST /evo-0/register (without cache)"
-curl -s -X POST http://localhost:8000/api/evo-0/register \
+curl -s -X POST "$BASE_URL/api/evo-0/register" \
     -H 'Content-type: application/json' \
     -d '{"name": "John Doe", "email": "johndoe@mail.com", "password": "secret", "password_confirmation": "secret"}'
 echo ""
 
 echo ""
 echo "[evo] Running ab on POST /evo-0/register (without cache)"
-ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST http://localhost:8000/api/evo-0/register | grep 'Requests per second'
+ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST "$BASE_URL/api/evo-0/register" | grep 'Requests per second'
 
 echo ""
 echo "[evo] Running curl to POST /evo-99/register (without cache)"
-curl -s -X POST http://localhost:8000/api/evo-99/register \
+curl -s -X POST "$BASE_URL/api/evo-99/register" \
     -H 'Content-type: application/json' \
     -d '{"name": "John Doe", "email": "johndoe@mail.com", "password": "secret", "password_confirmation": "secret"}'
 echo ""
 
 echo ""
 echo "[evo] Running ab on POST /evo-99/register (without cache)"
-ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST http://localhost:8000/api/evo-99/register | grep 'Requests per second'
+ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST "$BASE_URL/api/evo-99/register" | grep 'Requests per second'
 
 echo ""
 echo "[evo] Caching routes"
@@ -37,23 +37,23 @@ php artisan route:cache
 
 echo ""
 echo "[evo] Running curl to POST /evo-0/register (with cache)"
-curl -s -X POST http://localhost:8000/api/evo-0/register \
+curl -s -X POST "$BASE_URL/api/evo-0/register" \
     -H 'Content-type: application/json' \
     -d '{"name": "John Doe", "email": "johndoe@mail.com", "password": "secret", "password_confirmation": "secret"}'
 echo ""
 
 echo ""
 echo "[evo] Running ab on POST /evo-0/register (with cache)"
-ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST http://localhost:8000/api/evo-0/register | grep 'Requests per second'
+ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST "$BASE_URL/api/evo-0/register" | grep 'Requests per second'
 
 echo ""
 echo "[evo] Running curl to POST /evo-99/register (with cache)"
-curl -s -X POST http://localhost:8000/api/evo-99/register \
+curl -s -X POST "$BASE_URL/api/evo-99/register" \
     -H 'Content-type: application/json' \
     -d '{"name": "John Doe", "email": "johndoe@mail.com", "password": "secret", "password_confirmation": "secret"}'
 echo ""
 
 echo ""
 echo "[evo] Running ab on POST /evo-99/register (with cache)"
-ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST http://localhost:8000/api/evo-99/register | grep 'Requests per second'
+ab -q -p register-data.json -T application/json -c $NUM_CONCURRENCY -n $NUM_REQUEST "$BASE_URL/api/evo-99/register" | grep 'Requests per second'
 
